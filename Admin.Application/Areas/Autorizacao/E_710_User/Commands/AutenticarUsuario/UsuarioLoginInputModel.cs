52ecd9a4be71ca2ed8ebaa9e705a775088334061
evertonflois <38539481+evertonflois@users.noreply.github.com>
2024-12-13 20:00:56 -0300

    Delete Admin.Application/Areas/Autorizacao/E_710_User directory

--- Admin.Application/Areas/Autorizacao/E_710_User/Commands/AutenticarUsuario/UsuarioLoginInputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Admin.Application.Areas.Autorizacao.E_710_User.Commands.AutenticarUsuario
-{
-    public class UsuarioLoginInputModel
-    {
-        public string login { get; set; }
-
-        public string senha { get; set; }
-    }
-}
